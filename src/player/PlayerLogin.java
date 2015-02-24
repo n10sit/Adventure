ccbc4e749008a9007767c89c67f32e1f0f9d967d
n10sit <johnmasters43@gmail.com>
2015-02-23 19:55:43 -0600

    login

--- src/player/PlayerLogin.java
+++ src/player/PlayerLogin.java
@@ -1,7 +1,90 @@
 package player;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public  class PlayerLogin {
 	
+	public static void load(Player p, String name2, String pass2) {
+		String line = "";
+		String token = "";
+		String token2 = "";
+		boolean fileFound = true;
+		BufferedReader playerFile = null;
+		boolean endOfFile = false;
+		try {
+			playerFile = new BufferedReader(new FileReader("./players/"+name2+".txt"));
+		} catch (FileNotFoundException fnfe) {
+			fileFound = false;
+		}
+		if (!fileFound) {
+			p = new Player();
+			return;
+		} else {
+			try {
+				line = playerFile.readLine();
+			} catch(IOException ioexception) {
+				System.out.println(name2+": error loading file.");
+			}
+		}
+		while (!endOfFile && line != null) {
+			line = line.trim();
+			int spot = line.indexOf("=");
+			if (spot > -1) {
+				token = line.substring(0, spot);
+				token = token.trim();
+				token2 = line.substring(spot + 1);
+				token2 = token2.trim();
+				if (token.equals("pass")) {
+					if (pass2.equalsIgnoreCase(token2)) {
+						pass2.equals(token2);
+					} else {
+						System.out.println("Incorrect password.");
+						return;
+					}
+				}
+				if (token.equals("money")) {
+					p.money = Integer.parseInt(token2);
+				}
+			}
+			try {
+				line = playerFile.readLine();
+			} catch(IOException ioexception1) { 
+				endOfFile = true;
+			}
+		}
+		try {
+			playerFile.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
+	public static void save(Player p) {
+		if(p.name == null) {
+			return;
+		}
+		BufferedWriter playerFile = null;
+		try {
+			playerFile = new BufferedWriter(new FileWriter("./players/"+p.name+".txt"));
+			
+			playerFile.write("name = ", 0, 7);
+			playerFile.write(p.name, 0, p.name.length());
+			playerFile.newLine();
+			playerFile.write("money = ", 0, 8);
+			playerFile.write(Integer.toString(p.money), 0, Integer.toString(p.money).length());
+			playerFile.newLine();
+			playerFile.write("[EOF]", 0, 5);
+			playerFile.newLine();
+			playerFile.newLine();
+			playerFile.close();
+		} catch (IOException oops) {
+			return;
+		}
+	}
 	
 }
