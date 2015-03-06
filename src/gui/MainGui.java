6e15c9ea42a94c42ee2491ff6a1813c7c951ba32
n10sit <johnmasters43@gmail.com>
2015-03-06 14:30:20 -0600

    fixed the weird gui stuff i tried

--- src/gui/MainGui.java
+++ src/gui/MainGui.java
@@ -17,48 +17,37 @@ public class MainGui extends JPanel implements ActionListener {
 	
 	public MainGui(Player player) {
 		this.p = player;
+		setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+			
+		JPanel walk = new WalkingGui();
+		gbc.weightx = 0.5;
+		gbc.weighty = 0.5;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		add(walk, gbc);
+			
+		JPanel map = new MapGui(p);
+		gbc.weightx = 0.5;
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		add(map, gbc);
+			
+		JButton save = new JButton("Save");
+		gbc.weightx = 0.5;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		add(save, gbc);
+		save.addActionListener(this);
+		
 	}
-	
-	class MainGui2 extends JPanel implements ActionListener {
-		public MainGui2() {
-			setLayout(new GridBagLayout());
-			GridBagConstraints gbc = new GridBagConstraints();
-			
-			JPanel walk = new WalkingGui();
-			gbc.weightx = 0.5;
-			gbc.weighty = 0.5;
-			gbc.fill = GridBagConstraints.BOTH;
-			gbc.gridx = 0;
-			gbc.gridy = 0;
-			add(walk, gbc);
-			
-			JPanel map = new MapGui(p);
-			gbc.weightx = 0.5;
-			gbc.fill = GridBagConstraints.BOTH;
-			gbc.gridx = 1;
-			gbc.gridy = 0;
-			add(map, gbc);
-			
-			JButton save = new JButton("Save");
-			gbc.weightx = 0.5;
-			gbc.fill = GridBagConstraints.HORIZONTAL;
-			gbc.gridx = 1;
-			gbc.gridy = 1;
-			add(save, gbc);
-			save.addActionListener(this);
-		}
-
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			PlayerLogin.save(p);
 			System.out.println("Saved!!!");
 		}
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	
 }
